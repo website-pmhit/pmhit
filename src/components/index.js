466b7f195379054066be9d9f5f672b77084264e7
spashii <sameer.pashikanti@gmail.com>
2020-08-31 02:19:56 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Layout from './Layout';
+import Header from './Header';
+import Footer from './Footer';
+
+export { Layout, Header, Footer };
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Layout } from '../components';
+
+const Home = () => {
+  return <Layout>Home</Layout>;
+};
+
+export default Home;
