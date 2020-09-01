e1752ed4047d1faf4243e167ef73e17e355b984a
spashii <sameer.pashikanti@gmail.com>
2020-09-02 02:27:30 +0530

    relative images flaaback

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
 module.exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
+  fmImagesToRelative(node);
   if (node.internal.type == 'MarkdownRemark') {
     const value = createFilePath({ node, getNode });
     createNodeField({
