2c0eb251261363ab85582c44bc511966daa11f75
spashii <sameer.pashikanti@gmail.com>
2020-09-02 02:02:25 +0530

    netlify-cms setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,42 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'assets',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/content/blog`,
+        name: 'content-blog',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              include: ['image'],
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1024,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+  ],
+};
---
+++ src/templates/blog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const BlogPost = () => {
+  return <div> blog</div>;
+};
+
+export default BlogPost;
